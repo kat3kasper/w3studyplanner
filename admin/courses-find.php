522c134f6df6ed68956ece3c3018fd3f3affddd7
Ucchishta <ucc.men@gmail.com>
2013-04-05 13:17:25 -0400

    design change and form functionality

--- admin/courses-find.php
+++ admin/courses-find.php
@@ -15,13 +15,17 @@
 				window.close();
 				return false;
 			}
+			
+			function closeWin()
+			{
+				window.close();
+				return false;
+			}
 		</script>
 		
 	</head>
 	<body>
-			
 		<div class="container">
-		
 <?php
 	//If form is submitted & at least one field is filled
 	if(isset($_POST["search"]) && (!empty($_POST["course"]) || !empty($_POST["coursename"]) || !empty($_POST["department"])))
@@ -88,12 +92,25 @@
 		$sth->execute();
 		$rownum = $sth->rowCount();
 		
-		echo "Results: " . $rownum . " courses.<br/>\n";
-		
 		if(!$rownum)
-			echo "Course doesn't exist in database.<br/>\n";
+		{
+			?>
+			<div class="alert alert-error">
+				<button type="button" class="close" data-dismiss="alert"></button>
+				<h5>Course doesn't exist in database</h5>
+			</div>
+			<button type="button" class="btn btn-danger btn-large pull-right" onclick="closeWin()">Close</button>
+			<br><br>
+			<?php
+		}
 		else
 		{
+			?>
+			<div class="alert alert-success">
+				<button type="button" class="close" data-dismiss="alert"></button>
+				<h5><?php echo "Results: " . $rownum . " course(s)"; ?></h5>
+			</div>
+			<?php
 			//while($rownum-- > 0)
 			while($row = $sth->fetch(PDO::FETCH_ASSOC))
 			{
@@ -106,66 +123,107 @@
 				$dept = $row["department"]; //$row[4];
 				$oc = $row["on_campus_semesters"]; //$row[5];
 				$wc = $row["web_campus_semesters"]; //$row[6];
-				
-				echo "</p><b><a href=\"Javascript:SendValueToParent('" . $pre . $num . "');\">Select this course</a></b><br/>\n";
-				echo "Course prefix: " . $pre . "<br/>\n";
-				echo "Course number: " . $num . "<br/>\n";
-				echo "Credits: " . $cred . "<br/>\n";
-				echo "Course name: " . $name . "<br/>\n";
-				echo "Department: " . $dept . "<br/>\n";
-				echo "Term offered - On campus:" . $oc . "<br/>\n";
-				echo "Term offered - Web campus:" . $wc . "<br/>\n</p>";
+				?>
+				<div class="well">
+				<table class="table table-bordered table-condensed">		
+					<tbody>
+						<tr class="success">
+							<td colspan="2">
+							<?php echo "<h4>".$pre." ".$num."</h4>"; ?>
+							</td>
+						</tr>
+						<tr>
+							<td><b>Credits</b></td>
+							<td><?php echo $cred; ?></td>
+						</tr>
+						<tr>
+							<td><b>Course name</b></td>
+							<td><?php echo $name; ?></td>
+						</tr>
+						<tr>
+							<td><b>Department</b></td>
+							<td><?php echo $dept; ?></td>
+						</tr>
+						<tr>
+							<td><b>Term - On campus</b></td>
+							<td><?php echo $oc; ?></td>
+						</tr>
+						<tr>
+							<td><b>Term - Web campus</b></td>
+							<td><?php echo $wc; ?></td>
+						</tr>
+					</tbody>
+				</table>
+				<p class="pull-right"><button class="btn btn-primary" type="button" onclick="Javascript:SendValueToParent('<?php echo $pre . $num; ?>')">Select</button></p><br>
+				</div>
+				<?php
 			}
 		}
 	}
 	else
 	{
 ?>
-			
-			<h4>Find Courses</h4>
-			<p>Fill in at least one field to search the course catalog.<br/>
-			
-			</p>
-			
-			<form class="form-horizontal" action="courses-find.php" method="POST">
-				<div class="control-group">
-					<label class="control-label" for="Course">Course</label>
-					<div class="controls">
-						<input name="course" type="text" id="Course" class="input-small" placeholder="e.g. CS101" />
+			<div id="wrapper" class="fluid">
+				<div class="row-fluid">
+				<form id="frmOptions" method="post" class="well form-inline span12">
+					<h4>Find Courses</h4>
+					<div class="alert alert-info">
+						<button type="button" class="close" data-dismiss="alert"></button>
+						<p>Fill in at least <b>one</b> field to search the course catalog.<br/>
 					</div>
-				</div>					
-				<div class="control-group">
-					<label class="control-label" for="CourseName">Course Name</label>
-					<div class="controls">
-						<input name="coursename" type="text" id="CourseName" class="medium-small" placeholder="e.g. System Programming" />
+				 
+					<div class="row-fluid">   
+						<div id="formLeft" class="span2">
+							<div class="control-group">
+								<label class="control-label" for="Course">Course</label>
+								<div class="controls">
+									<input name="course" type="text" id="Course" class="input-small" placeholder="e.g. CS101" />
+								</div>
+							</div>
+						</div>
+						
+						<div id="formCenter" class="span3">
+							<div class="control-group">
+								<label class="control-label" for="CourseName">Course Name</label>
+								<div class="controls">
+									<input name="coursename" type="text" id="CourseName" class="medium-small" placeholder="e.g. System Programming" />
+								</div>
+							</div>   
+						</div>
+						
+						<div id="formRight" class="span4">
+						  <div class="control-group">
+							<label class="control-label" for="Department">Department</label>
+								<div class="controls">
+									<select name="department" class="medium-small">
+										<option value="" selected>Select a department..</option>
+										<option value="arts">Arts and Letters</option>
+										<option value="business">Business and Technology</option>
+										<option value="chemical">Chemical Engineering & Materials Science</option>
+										<option value="chemistry">Chemistry, Biology & Biomedical Engineering</option>
+										<option value="civil">Civil, Environmental & Ocean Engineering</option>
+										<option value="computer">Computer Science</option>
+										<option value="electrical">Electrical & Computer Engineering</option>
+										<option value="mathematical">Mathematical Science</option>
+										<option value="mechanical">Mechanical Engineering</option>
+										<option value="physics">Physics & Engineering Physics</option>
+										<option value="quantitative">Quantitative Finance</option>
+										<option value="systems">Systems & Enterprises</option>
+									</select>
+								</div>
+							</div>
+							
+						</div>  
+						
 					</div>
-				</div>				
-				<div class="control-group">
-					<label class="control-label" for="Department">Department</label>
-					<div class="controls">
-						<select name="department">
-							<option value="" selected>Select a department..</option>
-							<option value="arts">Arts and Letters</option>
-							<option value="business">Business and Technology</option>
-							<option value="chemical">Chemical Engineering & Materials Science</option>
-							<option value="chemistry">Chemistry, Biology & Biomedical Engineering</option>
-							<option value="civil">Civil, Environmental & Ocean Engineering</option>
-							<option value="computer">Computer Science</option>
-							<option value="electrical">Electrical & Computer Engineering</option>
-							<option value="mathematical">Mathematical Science</option>
-							<option value="mechanical">Mechanical Engineering</option>
-							<option value="physics">Physics & Engineering Physics</option>
-							<option value="quantitative">Quantitative Finance</option>
-							<option value="systems">Systems & Enterprises</option>
-						</select>
+					
+					<div class="form-actions">
+						<button type="submit" name="search" class="btn btn-primary">Find</button>
 					</div>
+					
+				</form>
 				</div>
-				<div class="control-group">
-					<div class="controls">
-						<button type="submit" name="search" class="btn btn-info">Search</button>
-					</div>
-				</div>
-			</form>
+			</div>
 			
 <?php
 	}
