fbd09299a28acd79e277f600947848eb97543375
Nabilah Farhanna Yahya <nabilah.farhana@gmail.com>
2013-04-18 14:29:19 -0400

    check type of user

--- index.php
+++ index.php
@@ -8,18 +8,45 @@
 		<?php require("includes/navigation.php"); ?>
 		
 		<div class="container">
-			<?php
+			 <?php
 				$member = substr($_SERVER["REDIRECT_unscoped_affiliation"],7);
-				echo "<p>Welcome, " . $_SERVER["REDIRECT_displayName"] . "<br/>You are currently logged in as " . $member . "</p>";
-				echo "<p>Use the site as:</p>";
+				$uid = $_SERVER["REDIRECT_uid"];
+				$arrAdmin = array("nyahya", "mabrahim", "kkaspero", "rsalas", "usivagur");
+
+				if($member == "student")
+				{
+					if(in_array($uid, $arrAdmin))
+					{
+						echo "<p>Welcome, " . $_SERVER["REDIRECT_displayName"] . ".<br/>";
+						echo "<p>Use the site as:</p>";				
 			?>
+						<p>
+							<a href="admin"><button class="btn btn-primary" type="button">Administrator</button></a>
+							<a href="student"><button class="btn" type="button">Student</button></a>
+						</p>
 			
-			<p>
-				<a href="admin"><button class="btn btn-primary" type="button">Administrator</button></a>
-				<a href="student"><button class="btn" type="button">Student</button></a>
-			</p>
-			
-			<p>You can also switch between the two from the navigation bar at the top.</p>
+						<p>You can also switch between the two from the navigation bar at the top.</p>
+			<?php									
+					}
+					else
+					{
+						header("Location: /student/index.php");
+					}
+				}
+				else
+				{	
+					echo "<p>Welcome, " . $_SERVER["REDIRECT_displayName"] . ".<br/>";
+					echo "<p>Use the site as:</p>";		
+			?>
+				<p>
+					<a href="admin"><button class="btn" type="button">Administrator</button></a>
+					<a href="student"><button class="btn" type="button">Student</button></a>
+				</p>
+				<p>You can also switch between the two from the navigation bar at the top.</p>
+			<?php	
+				}
+			?>
+
 			
 			<footer>
 				<p>© Study Planner 2013</p>
